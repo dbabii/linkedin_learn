90d6dcdf211770a89f83ff65313ebb5d09b779a8
Dima <dm.babii@gmail.com>
2025-07-09 14:04:42 +0100

    found a class by name

--- Ex_Files_Python_Automation_Testing_Upd/Exercise Files/CH02/02_03/test.py
+++ Ex_Files_Python_Automation_Testing_Upd/Exercise Files/CH02/02_03/test.py
@@ -15,13 +15,17 @@ login_form_xpath_abs = driver.find_element(By.XPATH, "/html/body/form[1]")
 login_form_xpath_rel = driver.find_element(By.XPATH, "//form[1]")
 # find a login form by XPath with relative path and using id
 login_form_xpath_id = driver.find_element(By.XPATH, "//form[@id='loginForm']")
+# find a class
+content = driver.find_element(By.CLASS_NAME, "content")
+
 # print the information into console
 print("My login form element is:", login_form)
 # print the output from the user
 print("Username:", username)
-
+# print login forms found in different ways
 print("Login forms:")
 print(login_form_xpath_abs)
 print(login_form_xpath_rel)
 print(login_form_xpath_id)
+print("Content:", content)
 driver.close()
