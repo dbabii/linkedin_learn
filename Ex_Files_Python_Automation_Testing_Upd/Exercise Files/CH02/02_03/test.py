75c091161341e065200bee601afc3e910bf9331c
Dima <dm.babii@gmail.com>
2025-07-09 14:01:24 +0100

    found an element in 3 different ways by XPath: - absolute; - relative; - relative by using id

--- Ex_Files_Python_Automation_Testing_Upd/Exercise Files/CH02/02_03/test.py
+++ Ex_Files_Python_Automation_Testing_Upd/Exercise Files/CH02/02_03/test.py
@@ -9,8 +9,19 @@ driver.get("file:///home/grom/git/linkedin_learn/Ex_Files_Python_Automation_Test
 login_form = driver.find_element(By.ID, "loginForm")
 # find an username on the page by name
 username = driver.find_element(By.NAME, "username")
+# find a login form by XPath with absolute path
+login_form_xpath_abs = driver.find_element(By.XPATH, "/html/body/form[1]")
+# find a login form by XPath with relative path
+login_form_xpath_rel = driver.find_element(By.XPATH, "//form[1]")
+# find a login form by XPath with relative path and using id
+login_form_xpath_id = driver.find_element(By.XPATH, "//form[@id='loginForm']")
 # print the information into console
 print("My login form element is:", login_form)
 # print the output from the user
 print("Username:", username)
+
+print("Login forms:")
+print(login_form_xpath_abs)
+print(login_form_xpath_rel)
+print(login_form_xpath_id)
 driver.close()
