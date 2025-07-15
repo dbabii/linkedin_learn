cea6f14bf38dc1897f98c830292ec6f806d0abad
Dima <dm.babii@gmail.com>
2025-07-15 19:28:45 +0100

    refactor last challenge. added Keys module.

--- Ex_Files_Python_Automation_Testing_Upd/Exercise Files/CH03/challenge_moin.py
+++ Ex_Files_Python_Automation_Testing_Upd/Exercise Files/CH03/challenge_moin.py
@@ -1,7 +1,8 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.select import Select
-import time
 '''
 1. Go to https://wiki.python.org/moin/FrontPage
 2. Perform a search for the text "Beginner"
@@ -11,15 +12,19 @@ import time
 driver = webdriver.Chrome()
 driver.get('https://wiki.python.org/moin/FrontPage')
 
-input_id = driver.find_element(By.ID, 'searchinput')
-time.sleep(1)
-input_id.clear()
-input_id.send_keys('Beginner')
-time.sleep(1)
-input_id.submit()
-time.sleep(1)
 
-drop_down = driver.find_element(By.XPATH, '//*[@id="sidebar"]/div[3]/ul/li[5]/form/div/select')
+input_id = driver.find_element(By.ID, 'searchinput')
+dropidXpath = '//*[@id="sidebar"]/div[3]/ul/li[5]/form/div/select'
+send_text = 'Beginner'
+
+input_id.clear()
+input_id.send_keys(send_text)
+#input_id.submit()
+# other solution
+input_id.send_keys(Keys.RETURN)
+time.sleep(5)
+
+drop_down = driver.find_element(By.XPATH, dropidXpath)
 # drop_down = driver.find_element(By.NAME, 'action')
 select = Select(drop_down)
 # select.select_by_index(2)
