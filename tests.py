5c055d3f8acff1d0c1b56ec404c7355c003e82a8
Dima <dm.babii@gmail.com>
2025-07-19 15:37:27 +0100

    added fixtures

--- tests.py
+++ tests.py
@@ -11,15 +11,23 @@ def test_to_date_exception():
     with pytest.raises(ValueError, match="12345 is not in YYYY-MM-DD format."):
         app._to_date("12345")
 
+# create a fixture
+@pytest.fixture
+def task_list():
+    return [Task(name="pay rent"), Task(name="buy bread")]
+
 @pytest.mark.parametrize("test_input, expected",
                          [("buy bread", Task(name="buy bread")),
                           ("buy banana", None),
                           ("PAY RENT", Task(name="pay rent")),
                               ])
-def test_find_task(test_input, expected):
-    task_list = [Task(name="pay rent"), Task(name="buy bread")]
+def test_find_task(test_input, expected, task_list):
     assert app._find_task(test_input, task_list) == expected
 
-def test_find_task_none():
-    task_list = [Task(name="pay rent"), Task(name="buy bread")]
+def test_find_task_none(task_list):
     assert app._find_task("buy banana", task_list) is None
+
+def test_save_load_task_list(task_list):
+    app._save_task_list(task_list)
+    load_list = app._get_task_list()
+    assert task_list == load_list
