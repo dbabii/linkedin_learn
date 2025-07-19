931185e5b255f1c8d69d33b96a9b31ee1f81a59c
Dima <dm.babii@gmail.com>
2025-07-19 15:57:27 +0100

    reformatted test.py via black

--- tests.py
+++ tests.py
@@ -2,31 +2,40 @@ import reminder as app
 from reminder import Task
 
 import datetime as dt
-import pytest 
+import pytest
+
 
 def test_to_date():
     assert app._to_date("2025-09-01") == dt.date(2025, 9, 1)
 
+
 def test_to_date_exception():
     with pytest.raises(ValueError, match="12345 is not in YYYY-MM-DD format."):
         app._to_date("12345")
+
 
 # create a fixture
 @pytest.fixture
 def task_list():
     return [Task(name="pay rent"), Task(name="buy bread")]
 
-@pytest.mark.parametrize("test_input, expected",
-                         [("buy bread", Task(name="buy bread")),
-                          ("buy banana", None),
-                          ("PAY RENT", Task(name="pay rent")),
-                              ])
+
+@pytest.mark.parametrize(
+    "test_input, expected",
+    [
+        ("buy bread", Task(name="buy bread")),
+        ("buy banana", None),
+        ("PAY RENT", Task(name="pay rent")),
+    ],
+)
 def test_find_task(test_input, expected, task_list):
     assert app._find_task(test_input, task_list) == expected
+
 
 @pytest.mark.skip(reason="not necessary atm")
 def test_find_task_none(task_list):
     assert app._find_task("buy banana", task_list) is None
+
 
 def test_save_load_task_list(task_list):
     app._save_task_list(task_list)
